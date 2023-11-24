f30cec4d6f649b38779c9a40288d3f1c287bb041
Israel Lima <israel_lima_16@hotmail.com>
2023-11-24 10:08:39 -0300

    first commit

---
+++ src/main/java/br/com/ilima/apibusiness/ApiBusinessApplication.java
@@ -0,0 +1,13 @@
+package br.com.ilima.apibusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ilima/apibusiness/ApiBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ilima.apibusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
